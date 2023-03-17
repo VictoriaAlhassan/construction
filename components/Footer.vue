c6c60d15a295d96dc6fbcdc07ddee6c8b6dd9d04
Victoria <victoria.alhassan@hubtel.com>
2023-03-17 09:36:59 -0700

    cleans the codebase

---
+++ components/Footer.vue
@@ -0,0 +1,149 @@
+<template>
+  <footer class="site-footer">
+    <div class="container">
+      <h3 class="footer-heading">Our Clients Say</h3>
+      <p class="footer-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
+        Suspendisse lectus tortor,<br />
+        dignissim sit amet, adipiscing nec, ultricies sed, dolor.
+      </p>
+      <div class="testimonial-section">
+        <div class="testimonial">
+          <div class="testimonial-content">
+            <img src="/client1.jpg" alt="Client 1" class="testimonial-img" />
+            <p class="testimonial-text">
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
+              risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
+              nec, ultricies sed, dolor."
+            </p>
+          </div>
+        </div>
+        <div class="testimonial">
+          <div class="testimonial-content">
+            <img src="/client2.jpg" alt="Client 2" class="testimonial-img" />
+            <p class="testimonial-text">
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
+              risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
+              nec, ultricies sed, dolor."
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-icons">
+      <div class="icon-container">
+        <div class="icon-wrapper">
+          <img class="icon" src="/address.png" alt="address" />
+          <div class="text">Address</div>
+          <p class="description">123 Main St, Anytown, USA</p>
+        </div>
+        <div class="icon-wrapper">
+          <img class="icon" src="/phone.png" alt="phone" />
+          <div class="text">Call Us</div>
+          <p class="description">+1-555-1234</p>
+        </div>
+        <div class="icon-wrapper">
+          <img class="icon" src="/email.png" alt="email" />
+          <div class="text">Email</div>
+          <p class="description">info@example.com</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style>
+.icon {
+  filter: saturate(50%) hue-rotate(150deg);
+  width: 37px;
+  height: 37px;
+}
+
+.site-footer {
+  background-color: #f2f2f2;
+  padding: 50px 0;
+  text-align: center;
+}
+
+.footer-heading {
+  font-size: 24px;
+  margin-bottom: 20px;
+}
+
+.footer-text {
+  font-size: 18px;
+  line-height: 1.5;
+  margin-bottom: 0;
+  margin-left: 25px;
+  margin-right: 25px;
+}
+
+.testimonial-section {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 30px;
+}
+
+.testimonial {
+  flex-basis: 45%;
+  max-width: 400px;
+}
+
+.testimonial-img {
+  border-radius: 45%;
+  display: block;
+  height: 90px;
+  margin: 0 auto 20px;
+  width: 100px;
+}
+
+.testimonial-text {
+  font-size: 18px;
+  line-height: 1.5;
+  margin-bottom: 0;
+}
+.testimonial-content {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  margin-bottom: 20px;
+}
+.footer-icons {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+}
+
+.icon-container {
+  margin-top: 35px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 80%;
+}
+
+.icon-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.icon-wrapper img {
+  width: 35px;
+  height: 35px;
+}
+
+.text {
+  text-align: center;
+  margin-top: 8px;
+}
+
+.description {
+  text-align: center;
+  margin-top: 4px;
+  font-size: 14px;
+  color: #888;
+}
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header class="bg-transparent">
+    <nav class="mx-auto max-w-7xl px-6 lg:px-8" aria-label="Top">
+      <div
+        class="flex w-full items-center justify-between border-b border-indigo-500 py-6 lg:border-none"
+      >
+        <div class="flex items-center color-white">
+          <h1>Propper.</h1>
+        </div>
+        <div class="ml-10 space-x-4">
+          <ul class="flex gap-4 color-white">
+            <li><NuxtLink to="/">Home</NuxtLink></li>
+            <li><NuxtLink to="/about">About</NuxtLink></li>
+            <li><NuxtLink to="/contact">Contact</NuxtLink></li>
+            <li><NuxtLink to="/gallery">Gallery</NuxtLink></li>
+            <li><NuxtLink to="/pricing">Pricing</NuxtLink></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+.router-link-exact-active {
+  text-decoration: underline;
+}
+
+header {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  /* padding: 20px; */
+  color: white;
+  text-shadow: 1px 1px 1px black;
+  z-index: 9999;
+}
+</style>
